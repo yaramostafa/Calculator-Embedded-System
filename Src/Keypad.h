61375d2dffa6821c3db1a75a491130dd5ccc3770
DedRec <89926533+DedRec@users.noreply.github.com>
2022-12-25 14:45:04 +0200

    Negative numbers and other features added
    Error handling and blocking functions

--- Src/Keypad.h
+++ Src/Keypad.h
@@ -4,10 +4,8 @@
 #include "types.h"
 #include "DIO.h"
 void KeyPad_Init(void);
-uint8 KeyPad_Read(void);
-uint16 GetDigits();
-void SevenSegment_Display();
-uint32 getDigits();
-void ReadInputs();
+uint8 KeyPad_Read_task1(void);
+uint32 getDigits_task1(uint8_ptr op, uint16 firstflag);
+uint8 ReadInputs_task1();
 
 #endif
--- Src/main.c
+++ Src/main.c
@@ -8,13 +8,37 @@
 #include "LCD.h"
 #include <stdlib.h>
 
+
 int main()
-{ 
+{
+  //KeyPad_Init();
+  //uint8_ptr str = "I want pizza";    //Write any string you want to display on LCD
+ 
   KeyPad_Init();
   LCD_init();
-								
+                                                  
    while(1){
-    ReadInputs();
+    LCD_Cmd(0x01);
+    LCD_Cmd(0x80);               //Force the cursor to beginning of 1st line
+    delayMs(500);
+    LCD_Write_String("Choose 1)Calc ");
+    LCD_Cmd(0xC0);
+    LCD_Write_String("2)Tim 3)SP");
+     
+    uint8 x;
+    x = KeyPad_Read_task1();
+    delay(0.3);
+    if(x == '1'){
+      uint8 y = '1';
+      while(y != 'F'){
+        y = ReadInputs_task1();
+      }
+     }/*else if(x == '2'){
+       
+     }else if(x == '3'){
+       
+     }*/
+    
    }	
   
   return 0;
